43604e51832856b00116849bb592498284c29226
Hen Shalom <xdrakolax@gmail.com>
2019-06-15 16:49:08 +0300

    add interfaces folder

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import configConvertor from './configConvertor'
 import * as validation from './validation'
+import * as  interfaces from './interfaces'
 
-export { validation, configConvertor }
+
+export { validation, configConvertor, interfaces }
---
+++ src/interfaces/index.js
@@ -0,0 +1,4 @@
+import DataConnectorsAbstract from './DataConnectorsAbstract'
+
+
+export { DataConnectorsAbstract }
