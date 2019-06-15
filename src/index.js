58f719aa8366f3028c26c503b7aeeb40d4b264be
Hen Shalom <xdrakolax@gmail.com>
2019-06-15 16:55:26 +0300

    add common tests

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import configConvertor from './configConvertor'
+import commonTest from './tests'
 import * as validation from './validation'
 import * as  interfaces from './interfaces'
 
-
-export { validation, configConvertor, interfaces }
+export { validation, commonTest, configConvertor, interfaces }
