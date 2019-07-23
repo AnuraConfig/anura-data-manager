ee2dd80506c510797c3d1bc07c0119cbb0caf0b7
Hen Shalom <xdrakolax@gmail.com>
2019-07-23 23:40:28 +0300

    add defaultParse

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
-import configConvertor from './configConvertor'
+import configConvertor from './configParser/configConvertor'
+import paramterInject from './configParser/paramterInjector'
+import ConfigParser from './configParser/parserObject'
 import commonTest from './tests'
 import * as validation from './validation'
 import * as  interfaces from './interfaces'
 
-export { validation, commonTest, configConvertor, interfaces }
+const defaultParse = [
+  {
+    key: "data",
+    name: "parameter inject",
+    condition: true,
+    parse: ParamterInject
+  },
+  {
+    key: "data",
+    name: "config Convertor",
+    condition: (config, options) => !options.isRaw,
+    parse: (config, options) => configConvertor.getObject(config.data, config.type)
+  },
+]
+const generateBasicParser = (customParsers) => new ConfigParser([...defaultParse, ...customParsers])
+
+export { validation, commonTest, configConvertor, interfaces, ConfigParser, generateBasicParser }
