102e8264d5e8a9abd658876416a65482afba598b
Hen Shalom <xdrakolax@gmail.com>
2019-07-28 23:15:28 +0300

    add tests

--- test/configPraser/parserObject.js
+++ test/configPraser/parserObject.js
@@ -1,24 +1,9 @@
-import assert from "assert"
 import chai from "chai"
-import configConvertor from "../../src/configParser/configConvertor"
-import ParamterInject from "../../src/configParser/paramterInjector"
+import { defaultParse } from "../../src"
 import ConfigParser from "../../src/configParser/parserObject"
 
 const expect = chai.expect
-const defaultParse = [
-  {
-    key: "data",
-    name: "parameter inject",
-    condition: true,
-    parse: ParamterInject
-  },
-  {
-    key: "data",
-    name: "config Convertor",
-    condition: (config, options) => !options.isRaw,
-    parse: (config, options) => configConvertor.getObject(config.data, config.type)
-  },
-]
+
 
 describe('ConfigParser', function () {
   describe('#parseConfig', function () {
@@ -27,7 +12,7 @@ describe('ConfigParser', function () {
       this.newConfig = new ConfigParser(defaultParse)
     })
 
-    it('should parse config  ', function () {
+    it('should replace var and parse config', function () {
       const config = {
         data: "{\"serviceUrl\":\"@url\"}",
         type: 'JSON'
@@ -37,6 +22,18 @@ describe('ConfigParser', function () {
       }
       const parseConfig = this.newConfig.parseConfig(config, { globalVariables })
       expect(parseConfig).to.deep.equal({ data: { serviceUrl: 'http://someUrl' }, type: 'JSON' })
+    })
+
+    it('should only replace var', function () {
+      const config = {
+        data: "{\"serviceUrl\":\"@url\"}",
+        type: 'JSON'
+      }
+      const globalVariables = {
+        "url": "http://someUrl"
+      }
+      const parseConfig = this.newConfig.parseConfig(config, { globalVariables, isRaw: true })
+      expect(parseConfig).to.deep.equal({ data: "{\"serviceUrl\":\"http://someUrl\"}", type: 'JSON' })
     })
   })
 })
