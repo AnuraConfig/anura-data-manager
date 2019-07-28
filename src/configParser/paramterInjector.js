9571504350eaf055cedb0deb371af8a206798227
Hen Shalom <xdrakolax@gmail.com>
2019-07-28 23:09:07 +0300

    add defult paramter

--- src/configParser/paramterInjector.js
+++ src/configParser/paramterInjector.js
@@ -1,4 +1,7 @@
-export default function ParamterInject(config, { globalVariables, paramKey }) {
+import { DEFAULT_PARAMETER_KEY } from "../constants"
+
+
+export default function ParamterInject(config, { globalVariables, paramKey = DEFAULT_PARAMETER_KEY }) {
   let data = config.data
   Object.entries(globalVariables).forEach(([key, value]) => {
     data = data.replace(new RegExp(paramKey + key, 'g'), value)
